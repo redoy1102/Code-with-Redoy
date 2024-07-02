30c6d2343a646e025185e336c4b102d1fe08fab2
redoy1102 <hridoyvlog9@gmail.com>
2024-07-02 19:19:51 +0600

    Details page is opening of c private batch

---
+++ JS/Private Batch/Details/details_c_private.js
@@ -0,0 +1,7 @@
+function openHtmlPage() {
+    // for new window
+    window.open('../../.././HTML/Private Batch/Details/details_c_private.html', '_blank');
+
+    //for same window
+    // window.location.href = '../../.././HTML/Private Batch/Details/details_c_private.html';
+}
